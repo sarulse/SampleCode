4eed05c02ce34c16fc643b8c71dec2a50c2d2ace
sarulse <sarulselv@gmail.com>
2017-10-16 21:33:04 -0400

    Removed SearchPersonByPhoneNumberfolder from repository

--- SearchPersonByPhoneNumber/js/showRecord.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-        $('.results').each(function(index) {
-            $(".showDetail"+index).hide(); 
-            $("#showResult"+index).click(function(){                        
-                 $(".showDetail"+index).toggle(); 
-                  return false;                
-            });
-        });
-});
