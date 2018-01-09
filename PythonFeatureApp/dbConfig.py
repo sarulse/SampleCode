5415ea9004a19dc7fe36eec28117a4c482b8e60f
sarulse <sarulselv@gmail.com>
2018-01-09 00:35:03 -0500

    removed python feature app folder

--- PythonFeatureApp/dbConfig.py
+++
@@ -1,5 +0,0 @@
-
-# Connection string for db connection
-connection_strings = {
-    "main": "mysql+pymysql://sarul:test2006@127.0.0.1:3306/features"
-}
--- PythonFeatureApp/settings.py
+++
@@ -1,7 +0,0 @@
-DATABASE = {
-    'drivername': 'mysql+mysqlconnector:',
-    'host': '127.0.0.1',
-    'username': 'sarul',
-    'password': 'test2006',
-    'database': 'features'
-}
