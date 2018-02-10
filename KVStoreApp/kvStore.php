a3e54134226564322c5428ca703b12730e0e3231
sarulse <sarulselv@gmail.com>
2018-02-10 03:22:07 -0500

    Updated kv store.

--- KVStoreApp/kvStore.php
+++ KVStoreApp/kvStore.php
@@ -5,23 +5,46 @@
     class KVStore {
         private $keyName;
         private $valueName;
-        private $dataStore = array();        
-        
+                
         public function setKey($keyName, $valueName){
-            $this->dataStore[$keyName] = $valueName;            
+            $_SESSION[$keyName] = $valueName;
+            
         }
         
         public function getkey($keyName){
-            return $this->dataStore[$keyName];
-        }
+            return $_SESSION[$keyName];
+        }      
         
-    }  
+    }
+    //create KV store
+    $kv_store = new KVStore();
+        
+    //Store the new/updated value of the key in the store and print the store content
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        try {                
+                if ((empty($_POST['keyName'])) && (empty($_POST['valueName']))){
+                    throw new Exception('Could not create Key Value Store');  
+                }                                  
+                $key_name = htmlentities(strtolower($_POST['keyName']));
+                $value_name = htmlentities($_POST['valueName']);                
+                $kv_store->setKey($key_name, $value_name);                
+                                
+        }catch(Exception $e) {
+                echo 'Message: ' .$e->getMessage();
+        }
+        finally {
+            echo "<h4>\tKey:\t\t\t\t\t\tValue<br/></h4>";
+            ksort($_SESSION);            
+            foreach ($_SESSION as $key=> $val){
+                echo "\t".$key.":\t\t\t\t\t\t".$val."<br/>";
+            }    
+        }
+    }
       
     //To handle autocomplete key name suggestions   
     if (!empty($_GET['term'])){        
             if (!empty(array_keys($_SESSION))){            
-                $keySuggestions = array_keys($_SESSION);
-                $keys = array_values($keySuggestions);           
+                $keys = array_values(array_keys($_SESSION));           
                 echo json_encode($keys);                
             }             
     }     
@@ -31,33 +54,8 @@
         if (!empty($val)){                   
             echo json_encode($val);
         }           
-    }
+    }    
     
-    //Store the new/updated value of the key in the store and print the store content
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        try {                
-                if ((empty($_POST['keyName'])) && (empty($_POST['valueName']))){
-                    throw new Exception('Could not create Key Value Store');  
-                }    
-                $kvStore = new KVStore();
-                $key_name = htmlentities(strtolower($_POST['keyName']));
-                $value_name = htmlentities($_POST['valueName']);
-                $kvStore->setKey($key_name, $value_name);
-                $value = $kvStore->getkey($key_name);
-                //echo "Value: ",$value."\n";       
-                $_SESSION[$key_name]= $value;                
-                                
-        }catch(Exception $e) {
-                echo 'Message: ' .$e->getMessage();
-        }
-        finally {
-            echo "<h4>\tKey:\t\t\t\t\t\tValue<br/></h4>";
-            ksort($_SESSION);
-            foreach ($_SESSION as $key=> $val){
-                echo "\t".$key.":\t\t\t\t\t\t".$val."<br/>";
-            }    
-        }
-    }
     //session_destroy();
       
 ?>    
