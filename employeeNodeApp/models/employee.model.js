991412eb3f35112ca9aa81ee6b34ba5cc74b6671
Subha Arulselvam <sarulselvam@gmail.com>
2020-03-22 02:03:59 -0400

    Adding model file'

---
+++ employeeNodeApp/models/employee.model.js
@@ -0,0 +1,30 @@
+const static_employees = [{
+        "fname": "Alan",
+        "lname": "Turing",
+        "hdate": "2020-03-07",
+        "role": "Manager",
+        "empID": "2228000",
+        "quote": "History began July 4th, 1776. Anything before that was a mistake.",
+        "joke": "What did the 0 say to the 8? Nice belt."
+    },
+    {
+        "fname": "Albert",
+        "lname": "Einstein",
+        "hdate": "2020-02-05",
+        "role": "Manager",
+        "empID": "950089",
+        "quote": "History began July 4th, 1776. Anything before that was a mistake.",
+        "joke": "What did the 0 say to the 8? Nice belt."
+    },
+    {
+        "fname": "Abraham",
+        "lname": "Lincoln",
+        "hdate": "2020-03-07",
+        "role": "CEO",
+        "empID": "87699997",
+        "quote": "History began July 4th, 1776. Anything before that was a mistake.",
+        "joke": "What did the 0 say to the 8? Nice belt."
+    },
+];
+
+module.exports.static_employees = static_employees;
